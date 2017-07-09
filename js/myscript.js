8a149fb38777b9fa9a3f05f5c85cbc84dee88d20
Michael Caldwell <bigmic69@gmail.com>
2017-07-09 08:43:15 -0500

    Lost again

--- js/myscript.js
+++ js/myscript.js
@@ -33,30 +33,35 @@ if((age >= 5) && (age <= 6)){
 
 
 // SECTION START
-function openAlert(mess){
-  alert(mess);
+function openAlert(mess) {
+    alert(mess);
 }
 
 
-document.getElementById('newPic').onclick = function(event){
-  document.getElementById('newPic').src = "img/bike-1.jpg";
+document.getElementById('newPic').onclick = function(event) {
+    document.getElementById('newPic').src = "img/bike-1.jpg";
 }
-document.getElementById('newPic').onmouseover = function(event){
+document.getElementById('newPic').onmouseover = function(event) {
     document.getElementById('newPic').style.boxShadow = "4px 4px 10px #333";
 }
-document.getElementById('newPic').onmouseenter = function(event){
+document.getElementById('newPic').onmouseenter = function(event) {
     document.getElementById('newPic').style.borderStyle = "solid";
 }
-document.getElementById('newPic').onmouseenter = function(event){
+document.getElementById('newPic').onmouseenter = function(event) {
     document.getElementById('newPic').style.borderWidth = "thick";
 }
-document.getElementById('newPic').onmouseclick = function(event){
+document.getElementById('newPic').onmouseclick = function(event) {
     document.getElementById('newPic').style.borderColor = "9f0c0c";
 }
 
-document.getElementById('myH2').onmouseover = function(event){
-  document.getElementById('myH2').style.backgroundColor = "#444";
+document.getElementById('myH2').onmouseover = function(event) {
+    document.getElementById('myH2').style.backgroundColor = "#333";
 }
-document.getElementById('myH2').onmouseout = function(event){
-  document.getElementById('myH2').style.color = "#9f0c0c";
+document.getElementById('myH2').onmouseout = function(event) {
+    document.getElementById('myH2').style.color = "#9f0c0c";
 }
+
+
+$("p").on("mouseover", function() {
+    $(this).css("backgroundColor", "#555"),
+});
