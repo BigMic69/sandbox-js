bfe7b85654e36a6d673aa3c1116b9e726d9dbe5b
Michael Caldwell <bigmic69@gmail.com>
2017-12-01 14:15:53 -0600

    JS Commit

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@
 source "https://rubygems.org"
 
 # gem "rails"
+# Added at 2017-11-29:
+gem "compass", "~> 1.0"
 gem 'sass', '~> 3.4', '>= 3.4.25'
 gem 'materialize-sass', '~> 0.99.0'
--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,117 @@
+// HELLO BUTTON SECTION ==========
+var bod1 = document.getElementById('wrapMain');
+var btn1 = document.getElementById('myBtn');
+
+bod1.style.backgroundColor = '#333';
+btn1.style.color = '#fff';
+btn1.style.fontSize = '2em';
+btn1.style.fontFamily = 'Ubuntu'
+btn1.addEventListener('click', putText);
+
+function putText() {
+    document.getElementById('sayHello').innerHTML = '>' + ' What up doe???';
+}
+
+
+
+// HELLO LINKS SECTION ==========
+var colorLinks = document.getElementById('aLinks');
+var link1 = colorLinks.getElementsByTagName('a');
+colorLinks.style.backgroundColor = '#888';
+colorLinks.style.border = '1px solid #f8f8f8';
+colorLinks.style.marginBottom = '80px';
+for (var i = 0; i < link1.length; i++) {
+    link1[i].style.color = '#fff';
+    link1[i].style.backgroundColor = '#333';
+    link1[i].style.boxShadow = '1px 1px #f21515';
+    link1[i].style.padding = '10px';
+}
+
+link1[0].addEventListener('mouseenter', chgLink1);
+link1[1].addEventListener('mouseenter', chgLink2);
+link1[2].addEventListener('mouseenter', chgLink3);
+function chgLink1() {
+    link1[0].innerHTML = '> "HELLO!!!"';
+    link1[0].style.backgroundColor = '#f4f4f4';
+    link1[0].style.color = '#333';
+}
+function chgLink2() {
+    link1[1].innerHTML = '> "Ok-a-y!!!"';
+    link1[1].style.backgroundColor = '#f4f4f4';
+    link1[1].style.color = '#333';
+}
+function chgLink3() {
+    link1[2].innerHTML = '> "THIS HAS NOW CHANGED..."';
+    link1[2].style.backgroundColor = '#f4f4f4';
+    link1[2].style.color = '#333';
+}
+
+
+
+// JUMBOTRON SECTION ==========
+var jumbotron = document.getElementsByClassName('jumbotron');
+var txtContainer = document.getElementById('textContainer');
+var myTitle = document.getElementById('myH2');
+
+txtContainer.addEventListener('mouseenter', bgChange1);
+txtContainer.addEventListener('mouseleave', bgChange2);
+
+function bgChange1() {
+    txtContainer.style.backgroundColor = 'rgba(33, 33, 33, .9)';
+    txtContainer.style.color = '#f2f2f2';
+    txtContainer.style.textShadow = '1px 1px red';
+    txtContainer.style.border = '1px solid red';
+    txtContainer.style.fontSize = '1.5em';
+}
+function bgChange2() {
+    txtContainer.style.backgroundColor = 'rgba(33, 33, 33, .4)';
+    txtContainer.style.borderRadius = '20px';
+}
+
+
+
+// SMALL PIC BOX WITH BUTTON ==========
+
+var logoButton = document.getElementById('logoButton');
+logoButton.addEventListener('click', picFlip);
+function picFlip() {
+    var mixPic = document.getElementsByTagName('img');
+    var x = document.getElementById('protoPic');
+    // protoPic.className = 'hide';
+    if (x.className === 'protoPic') {
+        x.className = 'hide';
+    } else {
+        x.className = 'protoPic';
+    }
+
+}
+console.log(protoPic);
+
+
+// BOX SECTION ==========
+var boxList = document.querySelector('#boxSection');
+// console.log(boxList.children[0]);
+boxList.children[0].style.backgroundColor = '#333';
+boxList.children[0].style.color = '#f8f8f8';
+boxList.children[0].style.height = '100px';
+boxList.children[0].style.fontSize = '1.75em';
+// console.log(document.images);
+
+
+
+
+
+
+// var headerTitle = document.getElementById('myH2');
+// console.log(headerTitle);
+// headerTitle.style.fontSize = '1.5em';
+
+
+// aTags.style.color = 'red';
+// for (var i = 0; i < aTags.length; i++) {
+//     aTags[i].style.color = 'red';
+// }
+
 /*var yourName = prompt("What is you name?");
 
 if (yourName != null) {
@@ -62,18 +176,18 @@ document.getElementById('myH2').onmouseout = function(event) {
 }
 */
 
-$(document).ready(function() {
-    $(".text-container").on("mouseenter", function() {
-        $(this).toggleClass('textChg');
-    });
-
-    $('#logoButton').click(function() {
-        /*var div =$('.newPicWrap');
-        div.animate({left: '100px'}, "slow");
-        div.animate({width: '50%'}, "slow");*/
-        $('.newPicWrap').toggleClass('newPicChg');
-            //width: '50%'
-        //});
-    });
-
-})
+// $(document).ready(function() {
+//     $(".text-container").on("mouseenter", function() {
+//         $(this).toggleClass('textChg');
+//     });
+//
+//     $('#logoButton').click(function() {
+//         /*var div =$('.newPicWrap');
+//         div.animate({left: '100px'}, "slow");
+//         div.animate({width: '50%'}, "slow");*/
+//         $('.newPicWrap').toggleClass('newPicChg');
+//             //width: '50%'
+//         //});
+//     });
+//
+// })
